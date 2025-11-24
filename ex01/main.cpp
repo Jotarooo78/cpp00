41b6d237860342e3e5b30c8df6a9266352e99676
Armand Mosnier <armosnie@f4r2s8.clusters.42paris.fr>
2025-11-24 14:18:09 +0100

    ex00-ex02

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: armosnie <armosnie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/19 16:32:14 by armosnie          #+#    #+#             */
-/*   Updated: 2025/11/19 16:32:22 by armosnie         ###   ########.fr       */
+/*   Updated: 2025/11/24 14:17:14 by armosnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,13 @@ int main()
         std::cout << "waiting for commande [ADD], [SEARCH] or [EXIT]" << '\n';
         std::cin >> cmd;
         std::cin.ignore();
+        if (std::cin.eof()){
+            std::cout << "\nEOF detected, exiting..." << std::endl;
+            std::cin.clear();
+            break ;
+        }
         if (cmd == "")
-            return (0);
+            continue ;
         else if (cmd == "ADD")
             pb.add();
         else if (cmd == "SEARCH")
